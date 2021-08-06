eaed25be26b58fd57c82ef6c74b3c64dc037f875
dat <you@example.com>
2021-08-06 20:43:37 +0700

    customer authentication

--- ShopmeProject/ShopmeWebParent/ShopmeFontEnd/src/main/java/com/shopme/sercurity/WebSercurityConfig.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFontEnd/src/main/java/com/shopme/sercurity/WebSercurityConfig.java
@@ -2,10 +2,12 @@ package com.shopme.sercurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -18,11 +20,38 @@ public class WebSercurityConfig extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 	}
 	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new CustomerUserDetailsService();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+
+		return authProvider;
+	}	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().permitAll();
-
+		http.authorizeRequests()
+		.antMatchers("/customer").authenticated()
+		.anyRequest().permitAll()
+		.and()
+		.formLogin()
+			.loginPage("/login")
+			.usernameParameter("email")
+			.permitAll()
+		.and()
+		.logout().permitAll()
+		.and()
+		.rememberMe()
+			.key("1234567890_aBcDeFgHiJkLmNoPqRsTuVwXyZ")
+			.tokenValiditySeconds(14 * 24 * 60 * 60)
+		;			
 	
 }
 	@Override
